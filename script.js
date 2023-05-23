99a7ca1561bbfefeb8e26f57a0d3fe53bfcf9b4e
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-23 18:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
     //Write your code here
-
+ let a_tag= document.getElementsByTagName("a")[0]
+ a_tag.style.color="green";
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    let a_tag= document.getElementsByTagName("a")[0]
+    a_tag.style.color="black";
 }
+
